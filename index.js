5d37965b5ac758b382a595798bb6e8f34e15bff3
David Corwin <david.corwin@gsa.gov>
2020-05-07 07:54:50 -0700

    Add assertion on env var

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const assert = require('assert');
 const fs = require('fs');
+
 fs.mkdirSync('_site');
 fs.copyFileSync('index.html', '_site/index.html');
 console.log('Hello World!');
-console.log({ TEST_UEV: process.env.TEST_UEV })
+console.log({ TEST_UEV: process.env.TEST_UEV });
+assert.strictEqual(process.env.TEST_UEV, 'hello uev world!');
