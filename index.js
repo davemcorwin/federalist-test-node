f97626ae9462f4ff3026e07cb52d42c7d6dc4633
David Corwin <david.corwin@gsa.gov>
2020-07-21 09:33:15 -0700

    test env var

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 
 fs.mkdirSync('_site');
 fs.copyFileSync('index.html', '_site/index.html');
+console.log('MY ENV VAR: ' + process.env.MY_ENV_VAR);
 console.log('Hello World!');
 console.log({ TEST_UEV: process.env.TEST_UEV });
 assert.strictEqual(process.env.TEST_UEV, 'hello uev world!');
